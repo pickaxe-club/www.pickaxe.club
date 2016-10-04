19f55d726187d93764ed7c2e68648e635fed7efd
Carl Cravens <raven@phoenyx.net>
2016-10-04 10:26:46 -0400

    added duration report

--- generate.sh
+++ generate.sh
@@ -5,10 +5,15 @@ set -e
 cores=3
 
 rm -rf map-old
-time mapcrafter -c mapcrafter.cfg -j $cores
+start=$(date +%s)
+mapcrafter -c mapcrafter.cfg  -j $cores
+fin1=$(date +%s)
 find map-gen -name '*.jpg' |xargs -P $cores jpegoptim --strip-all
+fin2=$(date +%s)
 chmod -R o+rX map-gen
 mv map map-old
 mv map-gen map
 
+echo "map gen: $((($fin1-$start)/60)) min"
+echo "total gen: $((($fin2-$start)/60)) min"
 exit
